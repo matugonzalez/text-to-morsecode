9f5b4f404f6287e71b4280e4cfad330f46fdb33a
Mateo González <minidrw@gmail.com>
2024-10-08 23:26:32 -0300

    fix: error with morse code letter spacing

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,30 @@
 import { codeToTextRules, textToCodeRules } from "./rules"
+const removeSpace = (array) => {
+    let count = 0
+    return array.filter(item => {
+        if (item === ' ') {
+            count++
+            if (count === 3) {
+                count = 0
+                return false
+            } 
+        } else {
+            count = 0
+        }
+
+        return true
+    })
+}
 
 const convertToCode = (string) => {
     const array = string.split('')
 
     const morseArray = array.map((letter) => {
         return textToCodeRules(letter)
-    })
-    return morseArray.join(' ')
+w   })
+    const reducedMorse = removeSpace(morseArray.join(' ').split(''))
+
+    return reducedMorse.join('')
 }
 
 const convertToText = (string) => {
